0aa53759f088ba591031cbd525ac0eb0a090df85
Zhanar S <zhan-ct@yandex.ru>
2022-12-18 18:19:21 +0300

    добавлена программа по выведению четных чисел

---
+++ DZ_04_EvenNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа, которая на вход принимает число N, а на выходе показывает чётные числа от 1 до N
+
+Console.Write("Введите число = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int k = 1;
+
+while (k < N-2)
+{
+   if(k%2==0)
+{
+    Console.Write(k+", ");
+}
+k++; 
+}
+
+while (k < N)
+{
+if(k%2==0)
+{
+    Console.Write(k+".");
+}
+k++; 
+}
