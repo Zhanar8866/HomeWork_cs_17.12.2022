ba3579e19ef0e093f719632aa0765789cb233b07
Zhanar S <zhan-ct@yandex.ru>
2022-12-18 17:40:31 +0300

    добавлена программапо поиску Max и Min

---
+++ DZ_01_MaxMin/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+
+Console.Write("Введите число A = ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B = ");
+int B = Convert.ToInt32(Console.ReadLine());
+if(A>B)
+{
+    Console.WriteLine("max="+A);
+    Console.WriteLine("min="+B);
+}
+else
+{
+    Console.WriteLine("max="+B);
+    Console.WriteLine("min="+A);
+}
